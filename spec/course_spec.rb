09f121df567de33ca76947130dcdc773869ba976
Rafael La Buonora <rlabuonora@yahoo.com>
2015-09-29 16:50:09 -0300

    Fixed rspec infrastructure

---
+++ spec/course_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper.rb'
+
+describe Course do
+  subject(:course) { Course.new(name: "My Course", shortDescription: "bbla bla", smallIcon: "https://tag.com") }
+
+  it "has name, icon and description" do
+    expect(course.name).to eq("My Course")
+    expect(course.short_description).to eq("bbla bla")
+  end
+
+  it "raises an error for something out of the API" do
+    expect(course.nada).to raise_error
+  end
+end
+
+
+describe Course do
+  subject(:course) { Course.new(name: "My Course", shortDescription: "bbla bla", smallIcon: "https://tag.com") }
+
+  it "has name, icon and description" do
+    expect(course.name).to eq("My Course")
+    expect(course.short_description).to eq("bbla bla")
+  end
+end
+
+
+describe Course do
+  subject(:course) { Course.new(name: "My Course", shortDescription: "bbla bla", smallIcon: "https://tag.com") }
+
+  it "has name, icon and description" do
+    expect(course.name).to eq("My Course")
+    expect(course.short_description).to eq("bbla bla")
+  end
+end
